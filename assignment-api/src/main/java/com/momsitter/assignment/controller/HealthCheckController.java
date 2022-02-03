1fc21b480fcefe51f490b2db27ed52242db4fa6e
Hyeon9mak <jinha3507@gmail.com>
2022-02-03 12:05:17 +0900

    feat: 헬스체킹용 API 추가

---
+++ assignment-api/src/main/java/com/momsitter/assignment/controller/HealthCheckController.java
@@ -0,0 +1,16 @@
+package com.momsitter.assignment.controller;
+
+import static org.springframework.http.HttpStatus.OK;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public ResponseEntity<Void> healthChecking() {
+        return ResponseEntity.status(OK).build();
+    }
+}
